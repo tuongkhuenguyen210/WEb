20cefb0684f59b8baff40653e28963ffcce4a09e
tuongkhuenguyen210 <33782822+tuongkhuenguyen210@users.noreply.github.com>
2017-12-03 15:22:18 +0100

    Add files via upload

---
+++ Scroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+
+    $("#myNavbar a").on('click', function(event){
+
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
+});
